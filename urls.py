145afcb1a50e24b4c9ef78b931201364204f6224
Abu Zahrah <abuzahrah75@gmail.com>
2022-06-11 02:38:47 +0800

    add modal for detail, edit and delete

--- urls.py
+++ urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from .views import *
+from .views_htmx import *
 
 urlpatterns = [
     path('', index, name="tugasan-index"),
     path('add_tugas/', add_tugas, name="tugasan-add"),
+    # path('')
 
 ]
+
+htmx_urlpatterns =[
+    path('detail/<int:pk>', pop_detail, name='tugasan-pop-detail'),
+    path('edit/<int:pk>', pop_edit, name='tugasan-pop-edit'),
+    path('delete/<int:pk>', pop_delete, name='tugasan-pop-delete'),
+    path('delete-confirm/<int:pk>', pop_delete_confirm, name='tugasan-delete-confirm'),
+    path('empty', return_kosong, name="return-empty"),
+]
+
+urlpatterns += htmx_urlpatterns
---
+++ views_htmx.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from .models import Tugasan
+
+def return_kosong(request):
+    return render(request, 'tugasan/kosong.html')
+
+def pop_detail(request, pk):
+    tugasan = Tugasan.objects.get(id=pk)
+    return render(request, 'tugasan/partials/pop_detail.html', {'tugas': tugasan})
+
+
+def pop_edit(request, pk):
+    tugasan = Tugasan.objects.get(id=pk)
+    return render(request, 'tugasan/partials/pop_edit.html', {'tugas': tugasan})
+
+
+def pop_delete(request, pk):
+    tugasan = Tugasan.objects.get(id=pk)
+    return render(request, 'tugasan/partials/pop_delete.html', {'tugas': tugasan})
+
+
+def pop_delete_confirm(request,pk):
+    tugasan = Tugasan.objects.get(id=pk)
+    mytugas = tugasan.nama
+    tugasan.delete()
+    return render(request, 'tugasan/partials/delete_confirm_notify.html', {'tugas': mytugas})
